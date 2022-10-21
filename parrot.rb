c722621df61114af0db60165ff9ee7cfbda3ef57
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-21 14:19:04 +0300

    DONE

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot phrase = "Squawk!"
+    puts phrase
+    return phrase
+end
+#This lab is for testing my ability to control the return value of a method, not just what it does, but what it returns.
